3e94e3aa9a38d3b529f9c6977678c2ec2ab14bd8
uehara1414 <b1015062@fun.ac.jp>
2021-06-05 02:11:21 +0900

    Add sample code

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord
+import os
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+
+client = MyClient()
+client.run(os.environ['DISCORD_TOKEN'])
